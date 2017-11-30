ff49df830207dcdb344e97824ea9995b1f569f70
Michael Nieke <github@n1eke.de>
2017-11-30 14:56:43 +0100

    added activity for code solution

---
+++ app/src/main/java/apps/nieke/org/papas60er/Code.java
@@ -0,0 +1,16 @@
+package apps.nieke.org.papas60er;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mnieke on 30.11.2017.
+ */
+
+public class Code extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
